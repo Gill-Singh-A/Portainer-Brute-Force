c2b3d813280739bae646a1edf5ff73156693d8dc
Gill-Singh-A <amansg22@iitk.ac.in>
2024-07-05 00:37:05 +0530

    Added Command Line Arguments

--- main.py
+++ main.py
@@ -23,4 +23,61 @@ def get_arguments(*args):
     return parser.parse_args()[0]
 
 if __name__ == "__main__":
-    pass
+    arguments = get_arguments(('-t', "--target", "target", "Target Servers (Seperated by ',' or File Name)"),
+                              ('-u', "--users", "users", "Target Users (seperated by ',') or File containing List of Users"),
+                              ('-P', "--password", "password", "Passwords (seperated by ',') or File containing List of Passwords"),
+                              ('-c', "--credentials", "credentials", "Name of File containing Credentials in format ({user}:{password})"),
+                              ('-w', "--write", "write", "CSV File to Dump Successful Logins (default=current data and time)"))
+    if not arguments.target:
+        display('-', f"Please specify {Back.YELLOW}Target Server{Back.RESET}")
+        exit(0)
+    else:
+        try:
+            with open(arguments.target, 'r') as file:
+                arguments.targets = [target.strip() for target in file.read().split('\n') if target != '']
+        except FileNotFoundError:
+            arguments.target = arguments.target.split(',')
+        except Exception as error:
+            display('-', f"Error Occured while Reading File {Back.MAGENTA}{arguments.target}{Back.RESET} => {Back.YELLOW}{error}{Back.RESET}")
+    if not arguments.credentials:
+        if not arguments.users:
+            display('-', f"Please specify {Back.YELLOW}Target Users{Back.RESET}")
+            exit(0)
+        else:
+            try:
+                with open(arguments.users, 'r') as file:
+                    arguments.users = [user for user in file.read().split('\n') if user != '']
+            except FileNotFoundError:
+                arguments.users = arguments.users.split(',')
+            except:
+                display('-', f"Error while Reading File {Back.YELLOW}{arguments.users}{Back.RESET}")
+                exit(0)
+            display(':', f"Users Loaded = {Back.MAGENTA}{len(arguments.users)}{Back.RESET}")
+        if not arguments.password:
+            display('-', f"Please specify {Back.YELLOW}Passwords{Back.RESET}")
+            exit(0)
+        else:
+            try:
+                with open(arguments.password, 'r') as file:
+                    arguments.password = [password for password in file.read().split('\n') if password != '']
+            except FileNotFoundError:
+                arguments.password = arguments.password.split(',')
+            except:
+                display('-', f"Error while Reading File {Back.YELLOW}{arguments.password}{Back.RESET}")
+                exit(0)
+            display(':', f"Passwords Loaded = {Back.MAGENTA}{len(arguments.password)}{Back.RESET}")
+        arguments.credentials = []
+        for user in arguments.users:
+            for password in arguments.password:
+                arguments.credentials.append([user, password])
+    else:
+        try:
+            with open(arguments.credentials, 'r') as file:
+                arguments.credentials = [[credential.split(':')[0], ':'.join(credential.split(':')[1:])] for credential in file.read().split('\n') if len(credential.split(':')) > 1]
+        except:
+            display('-', f"Error while Reading File {Back.YELLOW}{arguments.credentials}{Back.RESET}")
+            exit(0)
+    if not arguments.write:
+        arguments.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}.csv"
+    display('+', f"Total Target Servers = {Back.MAGENTA}{len(arguments.target)}{Back.RESET}")
+    display('+', f"Total Credentials    = {Back.MAGENTA}{len(arguments.credentials)}{Back.RESET}")
