9e8008ebc289e5de2151c24ca76a2929a907068d
Gill-Singh-A <amansg22@iitk.ac.in>
2024-07-05 01:09:58 +0530

    Implemented Multithreaded Calling of Login Handler

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.firefox.options import Options
 from datetime import date
 from optparse import OptionParser
+from multiprocessing import Pool, Lock
 from colorama import Fore, Back, Style
 from time import strftime, localtime, time, sleep
 
@@ -25,6 +26,11 @@ def get_arguments(*args):
     for arg in args:
         parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
     return parser.parse_args()[0]
+
+lock = Lock()
+thread_count = 1
+headless_mode = True
+delay_check = 1
 
 def login(browser, target, username, password, delay=1):
     t1 = time()
@@ -53,7 +59,7 @@ def login(browser, target, username, password, delay=1):
         return False, t2-t1
     t2 = time()
     return True, t2-t1
-def loginHandler(thread_index, targets, credentials, delay, headless=True):
+def loginHandler(thread_index, targets, credentials, delay=1, headless=True):
     successful_logins = {}
     options = Options()
     options.headless = True if headless else False
@@ -63,11 +69,14 @@ def loginHandler(thread_index, targets, credentials, delay, headless=True):
             status, time_taken = login(browser, target, username, password, delay)
             if status == True:
                 successful_logins[target] = [username, password]
-                display(' ', f"Thread {thread_index+1}:{time_taken:.2f}s -> {Fore.CYAN}{username}{Fore.RESET}:{Fore.GREEN}{password}{Fore.RESET}@{Fore.MAGENTA}{target}{Fore.RESET} => {Back.MAGENTA}{Fore.BLUE}Authorized{Fore.RESET}{Back.RESET}")
+                with lock:
+                    display(' ', f"Thread {thread_index+1}:{time_taken:.2f}s -> {Fore.CYAN}{username}{Fore.RESET}:{Fore.GREEN}{password}{Fore.RESET}@{Fore.MAGENTA}{target}{Fore.RESET} => {Back.MAGENTA}{Fore.BLUE}Authorized{Fore.RESET}{Back.RESET}")
             elif status == False:
-                display(' ', f"Thread {thread_index+1}:{time_taken:.2f}s -> {Fore.CYAN}{username}{Fore.RESET}:{Fore.GREEN}{password}{Fore.RESET}@{Fore.MAGENTA}{target}{Fore.RESET} => {Back.RED}{Fore.YELLOW}Access Denied{Fore.RESET}{Back.RESET}")
+                with lock:
+                    display(' ', f"Thread {thread_index+1}:{time_taken:.2f}s -> {Fore.CYAN}{username}{Fore.RESET}:{Fore.GREEN}{password}{Fore.RESET}@{Fore.MAGENTA}{target}{Fore.RESET} => {Back.RED}{Fore.YELLOW}Access Denied{Fore.RESET}{Back.RESET}")
             else:
-                display(' ', f"Thread {thread_index+1}:{time_taken:.2f}s -> {Fore.CYAN}{username}{Fore.RESET}:{Fore.GREEN}{password}{Fore.RESET}@{Fore.MAGENTA}{target}{Fore.RESET} => {Fore.YELLOW}Error Occured : {Back.RED}{status}{Fore.RESET}{Back.RESET}")
+                with lock:
+                    display(' ', f"Thread {thread_index+1}:{time_taken:.2f}s -> {Fore.CYAN}{username}{Fore.RESET}:{Fore.GREEN}{password}{Fore.RESET}@{Fore.MAGENTA}{target}{Fore.RESET} => {Fore.YELLOW}Error Occured : {Back.RED}{status}{Fore.RESET}{Back.RESET}")
     browser.close()
     return successful_logins
 
@@ -76,6 +85,9 @@ if __name__ == "__main__":
                               ('-u', "--users", "users", "Target Users (seperated by ',') or File containing List of Users"),
                               ('-P', "--password", "password", "Passwords (seperated by ',') or File containing List of Passwords"),
                               ('-c', "--credentials", "credentials", "Name of File containing Credentials in format ({user}:{password})"),
+                              ('-H', "--headless", "headless", f"Headless Mode (True/False, Default={headless_mode})"),
+                              ('-d', "--delay", "delay", f"Delay Between checking of Form while Page Loading (Default={delay_check} seconds)"),
+                              ('-T', "--threads", "threads", f"Number of Threads (Default={thread_count})"),
                               ('-w', "--write", "write", "CSV File to Dump Successful Logins (default=current data and time)"))
     if not arguments.target:
         display('-', f"Please specify {Back.YELLOW}Target Server{Back.RESET}")
@@ -126,7 +138,31 @@ if __name__ == "__main__":
         except:
             display('-', f"Error while Reading File {Back.YELLOW}{arguments.credentials}{Back.RESET}")
             exit(0)
+    if arguments.headless == "True":
+        arguments.headless = True
+    else:
+        arguments.headless = headless_mode
+    if arguments.delay:
+        arguments.delay = float(arguments.delay)
+    else:
+        arguments.delay = delay_check
+    if arguments.threads:
+        arguments.threads = int(arguments.threads)
+    else:
+        arguments.threads = thread_count
     if not arguments.write:
         arguments.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}.csv"
-    display('+', f"Total Target Servers = {Back.MAGENTA}{len(arguments.target)}{Back.RESET}")
+    total_servers = len(arguments.targets)
+    display('+', f"Total Target Servers = {Back.MAGENTA}{total_servers}{Back.RESET}")
     display('+', f"Total Credentials    = {Back.MAGENTA}{len(arguments.credentials)}{Back.RESET}")
+    successful_logins = {}
+    pool = Pool(arguments.threads)
+    server_divisions = [total_servers[group*total_servers//arguments.threads: (group+1)*total_servers//arguments.threads] for group in range(arguments.threads)]
+    threads = []
+    display(':', f"Staring {Back.MAGENTA}{arguments.threads}{Back.RESET} Threads")
+    for index, server_division in enumerate(server_divisions):
+        threads.append(pool.apply_async(loginHandler, (index, server_division, arguments.credentials, arguments.delay, arguments.headless)))
+    for thread in threads:
+        successful_logins.update(thread.get())
+    pool.close()
+    pool.join()
